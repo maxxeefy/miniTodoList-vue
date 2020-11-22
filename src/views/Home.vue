747ca8cee57bf2e774499ef381ec23b27a240fe9
the-makros <maxxeefy@gmail.com>
2020-11-22 19:20:22 +0300

    Ending of miniTodoList

---
+++ src/views/Home.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h2>Home page</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque aliquid nostrum quaerat ut. Voluptates at necessitatibus iusto maiores placeat reiciendis. Voluptatem placeat et repudiandae eaque itaque dicta ratione quos provident.</p>
+
+        <router-link to="/todos">Todos</router-link>
+    </div>
+</template>
